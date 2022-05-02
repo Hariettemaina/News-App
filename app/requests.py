ac3b3645004aa97927484790846c64e9bff2e673
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-05-02 12:39:25 +0300

    funtion to get sources

--- app/requests.py
+++ app/requests.py
@@ -26,75 +26,75 @@ def get_sources():
 
     return sources_results
 
-def process_sources(source_list):
-    '''
-    Function  that processes the source results and transform them to a list of objects
+# def process_sources(source_list):
+#     '''
+#     Function  that processes the source results and transform them to a list of objects
     
-    Args:
-        source_list: A list of dictionaries that contain source details
+#     Args:
+#         source_list: A list of dictionaries that contain source details
     
-    Returns :
-        source_results: A list of source objects
-    '''
-    source_results = []
+#     Returns :
+#         source_results: A list of source objects
+#     '''
+#     source_results = []
 
-    for source_item in source_list:
-        id = source_item.get('id')
-        name = source_item.get('name')
-        description = source_item.get('description')
+#     for source_item in source_list:
+#         id = source_item.get('id')
+#         name = source_item.get('name')
+#         description = source_item.get('description')
 
-        source_object = Source(id,name,description)
+#         source_object = Source(id,name,description)
 
-        source_results.append(source_object)
+#         source_results.append(source_object)
 
-    return source_results
+#     return source_results
 
-def get_articles(id):
-    '''
-    Function to get a source and it's articles
-    '''
-    get_articles_url = 'https://newsapi.org/v1/articles?source={}&apiKey={}'.format(id,api_key)
+# def get_articles(id):
+#     '''
+#     Function to get a source and it's articles
+#     '''
+#     get_articles_url = 'https://newsapi.org/v1/articles?source={}&apiKey={}'.format(id,api_key)
 
-    with urllib.request.urlopen(get_articles_url) as url:
-        get_articles_data = url.read()
-        get_articles_response = json.loads(get_articles_data)
+#     with urllib.request.urlopen(get_articles_url) as url:
+#         get_articles_data = url.read()
+#         get_articles_response = json.loads(get_articles_data)
 
-        articles_results = None
+#         articles_results = None
 
-        if get_articles_response['articles']:
-            articles_results_list = get_articles_response['articles']
-            articles_results = process_articles(articles_results_list)
+#         if get_articles_response['articles']:
+#             articles_results_list = get_articles_response['articles']
+#             articles_results = process_articles(articles_results_list)
 
-    return articles_results
+#     return articles_results
 
-def process_articles(article_list):
-    '''
-    Function that processes the article results and transform them to a list of objects
+# def process_articles(article_list):
+#     '''
+#     Function that processes the article results and transform them to a list of objects
     
-    Args:
-        article_list: A list of dictionaries that contain article details
+#     Args:
+#         article_list: A list of dictionaries that contain article details
     
-    Returns :
-        article_results: A list of article objects
-    '''
-    article_results = []
+#     Returns :
+#         article_results: A list of article objects
+#     '''
+#     article_results = []
 
-    for article_item in article_list:
-        source = article_item.get('source')
-        title = article_item.get('title')
-        urlToImage = article_item.get('urlToImage')
-        description = article_item.get('description')
-        urlToArticle = article_item.get('url')
-        publishedAt = article_item.get('publishedAt')
+#     for article_item in article_list:
+#         source = article_item.get('source')
+#         title = article_item.get('title')
+#         urlToImage = article_item.get('urlToImage')
+#         description = article_item.get('description')
+#         urlToArticle = article_item.get('url')
+#         publishedAt = article_item.get('publishedAt')
 
-        if publishedAt != None:
+#         if publishedAt != None:
 
-            # Call publish_date_format method to convert date to a display-friendly format
-            date_to_display = Article.publish_date_format(publishedAt)
+#             # Call publish_date_format method to convert date to a display-friendly format
+#             date_to_display = Article.publish_date_format(publishedAt)
 
 
-            article_object = Article(source,title,urlToImage,description,urlToArticle,date_to_display)
+#             article_object = Article(source,title,urlToImage,description,urlToArticle,date_to_display)
 
-            article_results.append(article_object)
+#             article_results.append(article_object)
 
-    return article_results
+#     return article_results
