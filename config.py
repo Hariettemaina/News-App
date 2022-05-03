a301fbeb54ccfd2ef58b63e7b4424c13ed063cc4
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-05-03 10:02:20 +0300

    movwd files

--- config.py
+++ config.py
@@ -1,5 +1,31 @@
+
 class Config:
     '''
     General configuration parent class
     '''
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    NEWS_API_KEY = 'https://newsapi.org/v2/top-headlines/sources?apiKey=API_KEY{}'
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    
+    DEBUG = True
+    
+    
+#     config_options = {
+# 'development': DevConfig,
+# 'production': ProdConfig
+# }
