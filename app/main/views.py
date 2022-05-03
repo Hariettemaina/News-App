03370d55fee34d92c5e9e5e4f8dd6371175802e0
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-05-03 10:15:39 +0300

    installed bootstrap

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,21 @@
-from flask import render_template
-from app import app
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_sources,get_articles
 
-# Views
-@app.route('/')
+@main.route('/')
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('home.html')
+    title = "FlashNews - HomePage"
+    sources = get_sources()
+    return render_template('index.html', title=title, sources=sources)
+
+@main.route('/source/<source_id>')
+def source(source_id):
+    '''
+    View source page function that returns a source page and its data
+    '''
+    title = f"{source_id} page"
+    articles = get_articles(source_id)
+    return render_template('source.html', title=title, articles=articles)
